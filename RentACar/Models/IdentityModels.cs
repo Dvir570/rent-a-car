aa517cb233702b7f16d3fa5262811ab3968192a9
Nikola Majcen <nikola.majcen@gmail.com>
2015-09-14 22:28:07 +0200

    Changed primary key (string to int) and table names

--- RentACar/Models/IdentityModels.cs
+++ RentACar/Models/IdentityModels.cs
@@ -3,13 +3,14 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RentACar.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class MyUser : IdentityUser<int, MyUserLogin, MyUserRole, MyUserClaim>
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<MyUser, int> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
@@ -18,16 +19,67 @@ namespace RentACar.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class MyUserLogin : IdentityUserLogin<int> { }
+
+    public class MyUserClaim : IdentityUserClaim<int> { }
+
+    public class MyUserRole : IdentityUserRole<int> { }
+
+    public class MyRole : IdentityRole<int, MyUserRole>
+    {
+        public MyRole() { }
+
+        public MyRole(string name) { Name = name; }
+    }
+
+    public class MyUserStore : UserStore<MyUser, MyRole, int, MyUserLogin, MyUserRole, MyUserClaim>
+    {
+        public MyUserStore(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+
+    public class MyRoleStore : RoleStore<MyRole, int, MyUserRole>
+    {
+        public MyRoleStore(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<MyUser, MyRole, int, MyUserLogin, MyUserRole, MyUserClaim>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DefaultConnection")
         {
         }
 
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Map entities to tables
+            modelBuilder.Entity<MyUser>().ToTable("Users");
+            modelBuilder.Entity<MyRole>().ToTable("Roles");
+            modelBuilder.Entity<MyUserRole>().ToTable("UserRoles");
+            modelBuilder.Entity<MyUserLogin>().ToTable("UserLogins");
+            modelBuilder.Entity<MyUserClaim>().ToTable("UserClaims");
+
+            // Primary key autoincrement
+            modelBuilder.Entity<MyUser>().Property(p => p.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<MyRole>().Property(p => p.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<MyUserClaim>().Property(p => p.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            // Changed primary key names in tables
+            modelBuilder.Entity<MyUser>().Property(p => p.Id).HasColumnName("UserId");
+            modelBuilder.Entity<MyRole>().Property(p => p.Id).HasColumnName("RoleId");
+            modelBuilder.Entity<MyUserClaim>().Property(p => p.Id).HasColumnName("ClaimId");
         }
     }
 }
