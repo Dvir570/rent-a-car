fe107c7c4742a4770e183ba2091a32d39e30cc01
Nikola Majcen <nikola.majcen@gmail.com>
2015-09-16 02:16:12 +0200

    Added contact form and updated layout

--- RentACar/Controllers/HomeController.cs
+++ RentACar/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using RentACar.Models;
+using System.Net;
 
 namespace RentACar.Controllers
 {
@@ -22,9 +25,57 @@ namespace RentACar.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "If you have any question, feel free to ask. <br> We will answer as soon as possible.";
 
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Contact(Contact model)
+        {
+            model.Message = Request.Form["message"];
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                
+                // Message details
+                mail.From = new MailAddress(model.Email);
+                mail.Subject = model.Subject;
+                mail.IsBodyHtml = false;
+                mail.To.Add("RECEIVER EMAIL");
+
+                string sender = "Sender: " + model.Name + "(" + model.Email + ")" + "\n";
+                string subject = "Subject: " + model.Subject + "\n\n";
+                string body = "Message: " + model.Message + "\n\n";
+                string sendFrom = "Sent from: IN Car Rent";
+
+                mail.Body = sender + subject + body + sendFrom;
+
+                // Email configuration
+                // "Allow less secure apps" must be enable (Google Account)
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587 );
+                client.Credentials = new NetworkCredential("GMAIL USERNAME", "GMAIL PASSWORD");
+                client.EnableSsl = true;
+
+                // Sending email
+                client.Send(mail);
+                mail.Dispose();
+
+                ViewBag.Message = "<br> Your message has been sent successfuly.";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "<br> Your message has not been sent. Please try again. <br>";
+                return View();
+            }
         }
     }
 }
